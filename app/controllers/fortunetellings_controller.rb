254b9d4febc16bd614b684ced7a016b4120e36c0
Naoya-tech <naoya.techwork@gmail.com>
2019-03-08 20:23:55 +0900

    年齢を表示

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -6,6 +6,8 @@ class FortunetellingsController < ApplicationController
     @year = @birthday[0,4]
     @month = @birthday[4,2]
     @day = @birthday[6,2]
+    date_format = "%Y%m%d"
+    @age = (Date.today.strftime(date_format).to_i - @birthday.to_i) / 10000
     @constellation = choose_constellation(@birthday[4,4].to_i)
     if Date.today.strftime("%m%d") == @birthday[4,4]
       @message = "今日があなたの誕生日です。"
