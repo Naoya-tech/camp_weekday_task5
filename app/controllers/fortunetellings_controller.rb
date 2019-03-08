e7d5e5fbec31bfce3f03de4af799297a9fb85018
Naoya-tech <naoya.techwork@gmail.com>
2019-03-08 20:22:06 +0900

    星座を表示

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,14 +1,12 @@
 class FortunetellingsController < ApplicationController
+  include FortunetellingsHelper
 
   def show
     @birthday = params[:birthday]
     @year = @birthday[0,4]
     @month = @birthday[4,2]
     @day = @birthday[6,2]
-    
-    
-    
-    
+    @constellation = choose_constellation(@birthday[4,4].to_i)
     if Date.today.strftime("%m%d") == @birthday[4,4]
       @message = "今日があなたの誕生日です。"
     end
--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -1,2 +1,33 @@
 module FortunetellingsHelper
+  def choose_constellation(birth_m_d)
+                      case 
+                      
+                      when birth_m_d.between?(321, 419)
+                        "牡羊"
+                      when birth_m_d.between?(420, 520)
+                        "牡牛"
+                      when birth_m_d.between?(521, 621)
+                        "双子"
+                      when birth_m_d.between?(622, 722)
+                        "蟹"
+                      when birth_m_d.between?(723, 822)
+                        "獅子"
+                      when birth_m_d.between?(823, 922)
+                        "乙女"
+                      when birth_m_d.between?(923, 1023)
+                        "天秤"
+                      when birth_m_d.between?(1024, 1122)
+                        "蠍"
+                      when birth_m_d.between?(1123, 1221)
+                        "射手"
+                      when birth_m_d.between?(1222, 1231) || birth_m_d.between?(101, 119)
+                        "山羊"
+                      when birth_m_d.between?(120, 218)
+                        "水瓶"
+                      when birth_m_d.between?(219, 320)
+                        "魚"
+                      end
+  end
+
+  
 end
