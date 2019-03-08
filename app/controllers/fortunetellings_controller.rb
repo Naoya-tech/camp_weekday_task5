890216d85f6196dbde69bd6d0a41b2ce54997602
Naoya-tech <naoya.techwork@gmail.com>
2019-03-08 20:25:13 +0900

    運勢を表示

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -9,6 +9,7 @@ class FortunetellingsController < ApplicationController
     date_format = "%Y%m%d"
     @age = (Date.today.strftime(date_format).to_i - @birthday.to_i) / 10000
     @constellation = choose_constellation(@birthday[4,4].to_i)
+    @luck = ["大吉","中吉","吉","小吉","凶","大凶"].sample
     if Date.today.strftime("%m%d") == @birthday[4,4]
       @message = "今日があなたの誕生日です。"
     end
