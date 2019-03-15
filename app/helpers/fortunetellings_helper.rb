882884f1dd58ffcb5f3d833436a7223017906a4f
Naoya-tech <naoya.techwork@gmail.com>
2019-03-15 20:24:00 +0900

    軽微な修正

--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -30,6 +30,6 @@ module FortunetellingsHelper
   end
 
   def valid
-    Date.today.strftime("%Y%m%d").to_i >= @birthday.to_i && Date.valid_date?(@year.to_i, @month.to_i, @day.to_i) && @birthday.length == 8 
+    Date.today.strftime("%Y%m%d").to_i >= @birthday.to_i && Date.valid_date?(@birthday[0,4].to_i, @birthday[4,2].to_i, @birthday[6,2].to_i) && @birthday.length == 8 
   end
 end
